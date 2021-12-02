b78536c84f7539fca21d177754cc7f48ab9d4f83
Damien Sedgwick <damiensedgwick@icloud.com>
2021-12-02 19:31:24 +0000

    Day 2: Part One ✅

---
+++ 02/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func MakeMap(s string) map[string]int {
+	slice := strings.Fields(s)
+	key := slice[0]
+	value, _ := strconv.Atoi(slice[1])
+	m := map[string]int{key: value}
+
+	return m
+}
+
+func LoadInput(fileName string) []map[string]int {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatalf("Error opening file %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	file.Close()
+
+	var input []map[string]int
+
+	for _, val := range lines {
+		m := MakeMap(val)
+		input = append(input, m)
+	}
+
+	return input
+}
+
+func CalculateInstructions(input []map[string]int) map[string]int {
+	m := map[string]int{
+		"horizontal": 0,
+		"depth":      0,
+	}
+
+	for _, o := range input {
+		for key, value := range o {
+			switch key {
+			case "forward":
+				m["horizontal"] += value
+			case "down":
+				m["depth"] += value
+			case "up":
+				m["depth"] -= value
+			}
+		}
+	}
+
+	return m
+}
+
+func MultiplyPosition(m map[string]int) int {
+	return m["horizontal"] * m["depth"]
+}
+
+func PartOne() int {
+	input := LoadInput("input.txt")
+	sum := CalculateInstructions(input)
+	result := MultiplyPosition(sum)
+
+	fmt.Printf("Part One: %d \n", result)
+
+	return result
+}
+
+func main() {
+	PartOne()
+}
---
+++ 02/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeMap(t *testing.T) {
+	assert.Equal(t, map[string]int{
+		"Forward": 5,
+	}, MakeMap("Forward 5"))
+}
+
+func TestLoadInput(t *testing.T) {
+	assert.Equal(t, []map[string]int{
+		{
+			"forward": 5,
+		},
+		{
+			"down": 5,
+		},
+		{
+			"forward": 8,
+		},
+		{
+			"up": 3,
+		},
+		{
+			"down": 8,
+		},
+		{
+			"forward": 2,
+		},
+	}, LoadInput("input_test.txt"))
+}
+
+func TestCalculateInstructions(t *testing.T) {
+	input := LoadInput("input_test.txt")
+
+	assert.Equal(t, map[string]int{
+		"horizontal": 15,
+		"depth":      10,
+	}, CalculateInstructions(input))
+}
+
+func TestMultiplyPosition(t *testing.T) {
+	input := LoadInput("input_test.txt")
+	m := CalculateInstructions(input)
+
+	assert.Equal(t, 150, MultiplyPosition(m))
+}
